9e5a668e344462cde2a5af0df79d8358c86ea229
Taeyang Jin (Heli) <sun@dataportal.kr>
2022-11-30 01:17:10 +0900

    feat: 스프링부트 프로젝트 셋업

---
+++ member-invitation-interface/src/main/java/kr/dataportal/invitation/MemberInvitationApplication.java
@@ -0,0 +1,12 @@
+package kr.dataportal.invitation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberInvitationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemberInvitationApplication.class, args);
+    }
+}
---
+++ member-invitation-interface/src/test/java/kr/dataportal/invitation/MemberInvitationApplicationTests.java
@@ -0,0 +1,12 @@
+package kr.dataportal.invitation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberInvitationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
